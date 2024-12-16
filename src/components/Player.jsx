60cca3f67906ae7e62cf70738668b0e7c082b36c
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-12-16 18:01:19 +0000

    implement player name input functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [name, setName] = useState('unknown entity');
+  const [inputName, setInputName] = useState('');
+
+  function handleChange(e) {
+    setInputName(e.target.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {name}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange}/>
+        <button onClick={() => setName(inputName)}>Set Name</button>
       </p>
     </section>
   );
