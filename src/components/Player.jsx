653937929a5c23d4f96ae2112ac029d20b535be1
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-12-16 18:04:45 +0000

    refactor Player component to use separate handleClick function for setting name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -8,12 +8,16 @@ export default function Player() {
     setInputName(e.target.value);
   }
 
+  function handleClick() {
+    setName(inputName);
+  }
+
   return (
     <section id="player">
       <h2>Welcome {name}</h2>
       <p>
         <input type="text" onChange={handleChange} value={inputName}/>
-        <button onClick={() => setName(inputName)}>Set Name</button>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
