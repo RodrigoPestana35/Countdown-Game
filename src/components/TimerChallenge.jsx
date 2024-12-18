ff1b79950bcc8db8d308ca28b14ebd98f17e30b2
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-12-18 19:21:25 +0000

    implement TimerChallenge component with start/stop functionality and time expiration message

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,34 @@
+import { useState } from 'react';
+
 export default function TimerChallenge({ title, targetTime }) {
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timeExpired, setTimeExpired] = useState(false);
+
+    function handleStart(){
+        setTimerStarted(true);
+        setTimeout(() => {
+            setTimeExpired(true);
+        }, 1000 * targetTime);
+    }
+
+    function handleStop(){
+        
+    }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timeExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button>
-            Start Challenge
+        <button onClick={handleStart}>
+            {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
-      <p className="">
-        Time is running... / Timer inactive
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
       </p>
     </section>
   );
