b70c91cb71cb2b74c9451c2b1a833a860374efba
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-12-20 20:12:20 +0000

    refactor ResultModal and TimerChallenge to display remaining time and handle reset functionality

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,10 @@
 import { useImperativeHandle, useRef } from "react";
 
-export default function ResultModal({ result, targetTime, ref }) {
+export default function ResultModal({ remainingTime, targetTime, ref, onReset }) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2); //o toFixed(2) é para limitar a 2 casas decimais
 
   useImperativeHandle(ref, () => {
     return {
@@ -13,14 +16,14 @@ export default function ResultModal({ result, targetTime, ref }) {
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}!</h2>
+      {userLost && <h2>You Lost!</h2>}
       <p>
         The target time was <strong>{targetTime} seconds</strong>
       </p>
       <p>
-        You stoped the timer with <strong>X seconds left.</strong>
+        You stoped the timer with <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
